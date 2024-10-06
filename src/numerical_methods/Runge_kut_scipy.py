f18ff7581ba1475f6ec33e5c3d25a51c8ddf90e1
Petrocyan <Sokolovpv@inbox.ru>
2024-10-06 18:58:36 +0300

    new_params

--- src/numerical_methods/Runge_kut_scipy.py
+++ src/numerical_methods/Runge_kut_scipy.py
@@ -51,8 +51,8 @@ if __name__ == "__main__": # pragma: no cover
     plt.plot(t, sol.y[0, :], 'r')
     plt.xlabel('t')
     plt.ylabel('x(t)')
-    dl = (max(sol.y[0]) - min(sol.y[1]))*0.132 # <- change the last parameter to align the labels
-    plt.text(t[-1]/2-2.5, max(sol.y[0, :])+1.3*dl, f"omega={'' if l==1 else l}pi")
+    dl = (max(sol.y[0]) - min(sol.y[1]))*0.012 # <- change the last parameter to align the labels
+    plt.text(t[-1]/2-2.8, max(sol.y[0, :])+1.3*dl, f"omega={'' if l==1 else l}pi")
     plt.text(t[-1]/2, max(sol.y[0])+1.6*dl, f"gamma={gamma}")
     plt.text(t[-1]/2, max(sol.y[0])+dl, f"delta={delta}")
     plt.text(t[-1]/2+2.5, max(sol.y[0])+1.6*dl, f"alpha={alpha}")
